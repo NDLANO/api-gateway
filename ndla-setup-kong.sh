d6e0a1e1551a717429b8f60c0f70f7e507421271
Rune Storløpa <rst@knowit.no>
2016-06-16 12:08:55 +0200

    NDLA-301: Warning message of a bit crude script-handling

--- ndla-setup-kong.sh
+++ ndla-setup-kong.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # Makes justifications in kong.yml to configure it to the rest of the environment
+
 mv /etc/kong/kong.yml /etc/kong/kong.yml.bak
+
+### BEWARE...
+# This line removes all info in kong.yml before the 'nginx:'-element.
+# This might not be what you want if the structure of the file changes. (It works fine for ver. 0.8.3)
+###
 sed '/^nginx:/,$!d' /etc/kong/kong.yml.bak > /etc/kong/kong.yml
+
 echo -e '\ndatabase: "postgres"\npostgres:\n  host: "'$DATABASE_HOST'"\n  port: 5432\n  database: '$DATABASE_NAME'\n  user: "'$DATABASE_USER'"\n  password: "'$DATABASE_USER_PASSWORD'"\n' >> /etc/kong/kong.yml
