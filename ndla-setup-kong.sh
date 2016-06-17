d72e0f0a9d3fc12027565576dce8e93c33c83b17
Rune Storløpa <rst@knowit.no>
2016-06-17 12:38:55 +0200

    NDLA-301: Oppdatert etter code review

--- ndla-setup-kong.sh
+++ ndla-setup-kong.sh
@@ -9,4 +9,11 @@ mv /etc/kong/kong.yml /etc/kong/kong.yml.bak
 ###
 sed '/^nginx:/,$!d' /etc/kong/kong.yml.bak > /etc/kong/kong.yml
 
-echo -e '\ndatabase: "postgres"\npostgres:\n  host: "'$DATABASE_HOST'"\n  port: 5432\n  database: '$DATABASE_NAME'\n  user: "'$DATABASE_USER'"\n  password: "'$DATABASE_USER_PASSWORD'"\n' >> /etc/kong/kong.yml
+# This is manipulating a yaml file. Beware of the blanks!!
+echo -e '\n\ndatabase: "postgres"' >> /etc/kong/kong.yml
+echo -e '\npostgres:'  >> /etc/kong/kong.yml
+echo -e '  host: "'$DATABASE_HOST'"'  >> /etc/kong/kong.yml
+echo -e '  port: 5432'  >> /etc/kong/kong.yml
+echo -e '  database: "'$DATABASE_NAME'"'  >> /etc/kong/kong.yml
+echo -e '  user: "'$DATABASE_USER'"'  >> /etc/kong/kong.yml
+echo -e '  password: "'$DATABASE_USER_PASSWORD'"' >> /etc/kong/kong.yml
