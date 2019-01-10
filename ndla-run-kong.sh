38a387e053fd0dc5fd6a4a5882fc6635ed02fda4
Henrik Nårstad <henrik.narstad@knowit.no>
2019-01-10 14:25:12 +0100

    dont handle secrets here any more

--- ndla-run-kong.sh
+++ ndla-run-kong.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
 function prepare_remote {
-    secretsfile="/tmp/secrets"
-    aws s3 --region eu-central-1 cp s3://$NDLA_ENVIRONMENT.secrets.ndla/api-gateway.secrets $secretsfile
-
     export KONG_CLUSTER_ADVERTISE=$HOST_IP:7946
-
-    export KONG_PG_HOST=$(cat $secretsfile | jq -r .META_SERVER)
-    export KONG_PG_PORT=$(cat $secretsfile | jq -r .META_PORT)
-    export KONG_PG_DATABASE=$(cat $secretsfile | jq -r .META_RESOURCE)
-    export KONG_PG_USER=$(cat $secretsfile | jq -r .META_USER_NAME)
-    export KONG_PG_PASSWORD=$(cat $secretsfile | jq -r .META_PASSWORD)
-
-    rm $secretsfile
 }
 
 function setup_logging {
